05f8ffa533c3aa71195220555942c8964d0ac72e
Brian <bgonz136@gmail.com>
2021-03-09 20:41:08 -0600

    Input Validation

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-8">
+    <p class="text-3xl mx-auto text-center m-16 tracking-wider font-thin">
+      Welcome {{ user?.displayName || 'New User' }}
+    </p>
+    <img class="h-60 mx-auto" src="../assets/home.svg" alt="Home Image" />
+  </div>
+</template>
+
+<script setup>
+import { user } from '../helpers/useAuth'
+</script>
---
+++ src/views/Secret.vue
@@ -0,0 +1,3 @@
+<template>
+  <img class="h-60 mx-auto mt-80" src="../assets/login.svg" alt="Home Image" />
+</template>
